8658d42aa46912af21923b7af845a4bfaa653098
ihsanalidev <ihsanaliwork@gmail.com>
2026-03-26 21:59:05 +0700

    Menambahkan HomeController dan halaman home

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Website Toko</title>
+</head>
+<body>
+
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/produk">Produk</a>
+    </nav>
+
+    <hr>
+
+    @yield('content')
+
+</body>
+</html>
